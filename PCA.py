21a2597903a41da5bad4d5a5062be6e3ac60acc2
johan <johan@nordbo-robotics.com>
2020-04-28 20:08:22 +0200

    First version. Simply extracted the files from the original project.

---
+++ PCA.py
@@ -0,0 +1,427 @@
+import pyreadr
+import random
+import numpy as np
+import math
+import scipy.linalg as scp
+import matplotlib.pyplot as plt
+import time
+import kNN
+import cv2
+
+
+
+#array = (array - array min) / (array max - array min)
+def normalize(data):
+    centered = data - np.min(data, axis=0)
+    normalized = centered / np.max(centered, axis=0)
+    return normalized
+
+
+#array = ( array - arraymean ) / array std dev
+def standardize(data):
+    centered = data - np.mean(data, axis=0)
+    standardized = centered / np.std(centered, axis=0)
+    return standardized
+
+def pareto_plot(sorted_data, x="", y ="", title="", as_percent=False, save_as=None):
+
+    sum_arr = []
+    part_sum = 0
+    for data in sorted_data:
+        part_sum += data
+        sum_arr.append(part_sum)
+    sum_arr = np.array(sum_arr)
+
+    plt.figure()
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.title(title)
+
+    if (as_percent):
+        y = np.arange(0, 101, step=5)
+        yp = [str(p) + "%" for p in y]
+
+        axis = plt.gca()
+        axis.set_ylim([0, 100])
+        plt.yticks(y, yp)
+
+    plt.bar(range(len(sorted_data)), sorted_data)
+    plt.plot(sum_arr, color='coral')
+
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(16, 12)
+
+    plt.savefig(save_as)
+
+
+#PCA class
+class PCA:
+
+    def __init__(self, data):
+        #PC = loadings_columns * obs_columns
+        self.loadings = []
+        self.lambdas = []
+
+        #reshape so each observation is a column
+        data_reshape = np.array(data).transpose()
+        data = None
+
+        #do SVD
+        self.loadings, self.lambdas, _ = scp.svd(data_reshape)
+
+    #transform data to PC space (reduced is an index) i should probably remove reduced from this one
+    def to_PC(self, observation, reduced=None):
+        if reduced == None:
+            reduced = self.loadings.shape[0]
+
+        observation_reshape = observation.transpose()
+        observation_PC = self.loadings[:reduced].dot(observation_reshape)
+        return observation_PC.transpose()
+
+
+    #transform data using multiple levels of a reduced PCA model. i should probably rename to to_PC_reduced
+    def to_PC_multi(self, observation, reduced=None):
+        if reduced == None:
+            return self.to_PC(observation)
+        else:
+            #find the indexes
+            var = self.lambdas/np.sum(self.lambdas)*100
+            sum_var = [np.sum(var[:p+1]) for p in range(len(var))]
+            index_dict = {}
+            s = 0
+            for model in reduced:
+                for PC_index in range(s, len(sum_var)):
+                    if sum_var[PC_index] >= model:
+                        index_dict[str(model)] = PC_index+1
+                        break
+
+            #works until here
+
+            #get the PCS
+            PC_data_dict = {}
+            for keys in index_dict.keys():
+                PC_data_dict[keys] = self.to_PC(observation, index_dict[keys])
+
+            return PC_data_dict
+
+    def from_PC(self, PC_data):
+        #PC data should be pxn, e should be pxp , restored should be pxp * pxn = pxn, which is transposed to nxp
+        #extend the PCs with zeros if necessary
+        if PC_data.shape[1] < self.loadings.shape[0]:
+            PC_data = np.concatenate((PC_data, np.zeros((PC_data.shape[0], self.loadings.shape[0] - PC_data.shape[1]))), axis=1)
+        return np.transpose(np.dot(np.linalg.inv(self.loadings), np.transpose(PC_data)))
+
+def task_2_1_1():
+    print("Start of task 2.1.1")
+    #read data
+    data, data_size = kNN.read_all_in("venv/flattened_all.Rds", True)
+
+
+    #do PCA on the data
+    myPCA = PCA(data[:, 1:])
+
+
+    #do a pareto chart of explained variance
+    variances = np.squeeze(myPCA.lambdas)
+    pareto_plot(variances[:30], x="PCS", y="Variance", title="PC variances", save_as="PC_var_absolute.png")
+    pareto_plot(np.sqrt(variances[:100]), x="PCS", y="Variance", title="PC variances", save_as="PC_stddev_absolute.png")
+
+    #normalized
+    variances = (variances/np.sum(variances))*100
+    pareto_plot(variances, x="PCS", y="Explained variance [%]", as_percent=True, title="PC variances [%]", save_as="PC_var_percent.png")
+
+
+#does a kNN loop to get mean and std dev accruacy. should be moved to kNN or modified to take any ML class
+def kNN_iterative_accuracy_test(data_dict, k, iterations, groups=None, verbose=False):
+
+    if groups==None:
+        groups=[50, 50]
+
+    accuracies = np.zeros((iterations, len(k)))
+    accuracies = np.squeeze(accuracies)
+    for i in range (0, iterations, 1):
+        for keys in data_dict.keys():
+            val, train = kNN.rnd_split(data_dict[keys], groups, verbose=False)
+            gt = val[:,0]
+            mykNN = kNN.kNN(train)
+            pred = mykNN.mass_arg_predict(val, k, verbose=False)
+            pred = np.squeeze(pred, axis=2)
+            accuracy, _, _ = kNN.evaluate(pred, gt)
+
+            accuracies[i] = accuracy
+    if verbose:
+        print("Mean accuracy of ", np.mean(accuracies, axis=0), "% and standard deviation ", np.std(accuracies, axis=0), "%, for k =", k)
+
+    return np.mean(accuracies), np.std(accuracies)
+
+
+def task_2_1_23():
+    print("Start of task 2.1.2/3")
+    #read data
+    data, data_size = kNN.read_all_in("venv/flattened_all.Rds", True)
+
+    #do PCA on the data
+    myPCA = PCA(data[:,1:])
+
+    #get 80%, 90%, 95%, 99% var PCs
+    var = myPCA.lambdas/np.sum(myPCA.lambdas)*100
+    sum_var = [np.sum(var[:p+1]) for p in range(len(var))]
+    index_dict = {"80" : None, "90" : None, "95" : None, "99" : None}
+    for p in range(len(sum_var)):
+        if (index_dict["80"] == None and sum_var[p] >= 80):
+            index_dict["80"] = p+1
+        elif (index_dict["90"] == None and sum_var[p] >= 90):
+            index_dict["90"] = p+1
+        elif (index_dict["95"] == None and sum_var[p] >= 95):
+            index_dict["95"] = p+1
+        elif (index_dict["99"] == None and sum_var[p] >= 99):
+            index_dict["99"] = p+1
+
+    print("Found splits:" , index_dict.items())
+
+    #transform data to PC space
+    PC_data = {}
+    for keys in index_dict.keys():
+        tmp = np.empty([data.shape[0], index_dict[keys]+1])
+        tmp[:, 0] = data[:, 0]
+        tmp[:, 1:] = myPCA.to_PC(data[:, 1:], index_dict[keys])
+        PC_data[keys] = tmp
+
+    k = range(1, 11)
+    for keys in PC_data.keys():
+        print("kNN for ", keys, "% variance explained")
+        val, train = kNN.rnd_split(PC_data[keys], [50, 50], verbose=False)
+        gt = val[:,0]
+        mykNN = kNN.kNN(train)
+        pred = mykNN.mass_arg_predict(val, k, verbose=False)
+        print(pred.shape, gt.shape)
+        pred = np.squeeze(pred, axis=2)
+        accuracy, _, _ = kNN.evaluate(pred, gt)
+        print(accuracy)
+
+        kNN.k_plot((keys + "% PC based kNN "), k, accuracy*100, yticks=(range(93, 101, 1)), ylabel="Accuracy [%]")
+
+    #do kNN 1-10 for 80%, 90%, 95% and 99% variance explained
+
+
+    #measure run times and compare
+
+
+def task_2_3_none(data):
+    print("using raw data:")
+
+    #do PCA on the data
+    myPCA = PCA(data[:,1:])
+
+    #get 80%, 90%, 95%, 99% var PCs
+    tmp = myPCA.to_PC_multi(data[:,1:], reduced=[80])
+
+    PC_data = {}
+    for keys in tmp.keys():
+        labels = np.expand_dims(data[:,0], axis=1)
+        PC_data[keys] = np.concatenate((labels, tmp[keys]), axis=1)
+
+    return kNN_iterative_accuracy_test(PC_data, [5], 10, groups=[50, 50], verbose=True)
+
+
+def task_2_2_nb(data):
+    print("using normalized data:")
+
+    #do normalization
+    data[:,1:] = normalize(data[:,1:])                                      #the magic
+
+    #do PCA on the data
+    myPCA = PCA(data[:,1:])
+
+    #get 80%, 90%, 95%, 99% var PCs
+    tmp = myPCA.to_PC_multi(data[:,1:], reduced=[80])
+
+    PC_data = {}
+    for keys in tmp.keys():
+        labels = np.expand_dims(data[:,0], axis=1)
+        PC_data[keys] = np.concatenate((labels, tmp[keys]), axis=1)
+
+    return kNN_iterative_accuracy_test(PC_data, [5], 10, groups=[50, 50], verbose=True)
+
+
+def task_2_2_na(data):
+    print("using normalized PC:")
+
+    #do PCA on the data
+    myPCA = PCA(data[:,1:])
+
+    #get 80%, 90%, 95%, 99% var PCs
+    tmp = myPCA.to_PC_multi(data[:,1:], reduced=[80])
+
+    PC_data = {}
+    for keys in tmp.keys():
+        labels = np.expand_dims(data[:,0], axis=1)
+        PC_data[keys] = np.concatenate((labels, normalize(tmp[keys])), axis=1)
+
+    return kNN_iterative_accuracy_test(PC_data, [5], 10, groups=[50, 50], verbose=True)
+
+
+def task_2_2_sb(data):
+    print("using standardized data:")
+
+    #do normalization
+    data[:,1:] = standardize(data[:,1:])
+
+    #do PCA on the data
+    myPCA = PCA(data[:,1:])
+
+    #get 80%, 90%, 95%, 99% var PCs
+    tmp = myPCA.to_PC_multi(data[:,1:], reduced=[80])
+
+    PC_data = {}
+    for keys in tmp.keys():
+        labels = np.expand_dims(data[:,0], axis=1)
+        PC_data[keys] = np.concatenate((labels, tmp[keys]), axis=1)
+
+    return kNN_iterative_accuracy_test(PC_data, [5], 10, groups=[50, 50], verbose=True)
+
+
+def task_2_2_sa(data):
+    print("using standardized PC")
+
+    #do PCA on the data
+    myPCA = PCA(data[:,1:])
+
+    #get 80%, 90%, 95%, 99% var PCs
+    tmp = myPCA.to_PC_multi(data[:,1:], reduced=[80])
+
+    PC_data = {}
+    for keys in tmp.keys():
+        labels = np.expand_dims(data[:,0], axis=1)
+        PC_data[keys] = np.concatenate((labels, standardize(tmp[keys])), axis=1)
+
+    return kNN_iterative_accuracy_test(PC_data, [5], 10, groups=[50, 50], verbose=True)
+
+
+def task_2_2():
+    print("Start of task 2.2")
+
+    #read data
+    data, data_size = kNN.read_all_in("venv/flattened_all.Rds", False)
+
+    accuracies = np.zeros(5)
+    stddev = np.zeros(5)
+    accuracies[0], stddev[0] = task_2_3_none(data.copy())
+    accuracies[1], stddev[1] = task_2_2_nb(data.copy())
+    accuracies[2], stddev[2] = task_2_2_na(data.copy())
+    accuracies[3], stddev[3] = task_2_2_sb(data.copy())
+    accuracies[4], stddev[4] = task_2_2_sa(data.copy())
+
+    plt.figure()
+    plt.bar(x=[1, 2, 3, 4, 5], height=accuracies*100, yerr=stddev*100, tick_label=["Raw", "Normalized data", "Normalized PCs", "Standardized data", "Standardized PCs"])
+    plt.tick_params(labelsize=20)
+    plt.ylim(bottom=92.5, top=100)
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(20, 12)
+    plt.title("Accuracy with data preparation (80% PC)", fontsize=20)
+    plt.ylabel("Accuracy [%]", fontsize=20)
+    plt.savefig("normalOrStandard_effect.png")
+    #plt.show()
+
+
+def task_2_3():
+    print("Start of task 2.3")
+
+   #read data
+    data, data_size = kNN.read_all_in("venv/flattened_all.Rds", False)
+
+    accuracies_smoothing = []
+    stddev_smoothing = []
+    acc, stddev = task_2_3_none(data.copy())
+    accuracies_smoothing.append(acc)
+    stddev_smoothing.append(stddev)
+
+    #get images
+    images = np.reshape(data[:,1:], (data_size[0], 18, 18))
+    images[:] = images.swapaxes(1, 2)
+
+    x = [0]
+    sigmas  = range(5, 151, 5)
+    for sigma in sigmas:
+        print("Sigma = ", sigma*0.01)
+        tmp = cv2.GaussianBlur(images[:], ksize=(5,5), sigmaX=sigma*0.01)
+        tmp_data = data.copy()
+        tmp_data[:,1:] = np.reshape(tmp, (data_size[0], data_size[1]-1))
+        acc, stddev = task_2_3_none(tmp_data.copy())
+        x.append(sigma*0.01)
+        accuracies_smoothing.append(acc)
+        stddev_smoothing.append(stddev)
+
+    x = np.array(x)
+    accuracies_smoothing = np.array(accuracies_smoothing)*100
+    stddev_smoothing = np.array(stddev_smoothing)*100
+
+
+    plt.figure()
+    plt.errorbar(x, accuracies_smoothing, stddev_smoothing, fmt='--')#, marker='s', mfc='red', mec='green', ms=20, mew=4)
+    plt.tick_params(labelsize=14)
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(16, 12)
+    plt.title("Accuracy with smoothing (80% PC)", fontsize=20)
+    plt.xlabel("Sigma", fontsize=20)
+    plt.ylabel("Accuracy [%]", fontsize=20)
+    plt.savefig("smoothing_effect.png")
+    #plt.show()
+
+def task_2_4():
+    print("Start of task 2.4")
+
+    #read data
+    data, data_size = kNN.read_all_in("venv/flattened_all.Rds", False)
+
+
+#plot one of each cipher
+    print("Drawing originals")
+    #get images
+    images = np.reshape(data[:,1:], (data_size[0], 18, 18))
+    images[:] = images.swapaxes(1, 2)
+
+    c = 0
+    for i in range(50, 4000, 400):
+        #cv2.imshow("cipher", images[i])
+        #cv2.waitKey()
+        cv2.imwrite("ciphers/cipher_" + str(c) + "_orig.png",  cv2.resize(images[i]*255, (144,144), interpolation=cv2.INTER_NEAREST))
+        c += 1
+
+
+#make them into PCA and back again, to show the lost information
+    myPCA = PCA(data[:,1:])
+
+    PC_data_dict = myPCA.to_PC_multi(data[:,1:], reduced=[80, 90, 95])
+    for keys in PC_data_dict:
+        print("Drawing PC" + keys)
+        restored_data = myPCA.from_PC(PC_data_dict[keys])
+        print(np.sum(restored_data - data[:,1:]), np.min(restored_data))
+        images = np.reshape(restored_data, (data_size[0], 18, 18))
+        c = 0
+        for i in range(50, 4000, 400):
+            #cv2.imshow("cipher", images[i])
+            #cv2.waitKey()
+            cv2.imwrite("ciphers/cipher_" + str(c) + "_" + str(keys) + "%_restored.jpg", cv2.resize(images[i]*255, (144,144), interpolation=cv2.INTER_NEAREST))
+            c += 1
+
+#take 10 eigenvectors
+#transform them to data
+    #make that data into images
+
+    #reconstruct the same 10 images using 80, 90, 95
+    #show the images
+
+    #compare two different ciphers in PC space
+    #compare the mean of those two ciphers in PC space
+    #compare to loadings
+
+
+
+###main
+task_2_1_1()
+task_2_1_23()
+task_2_2()
+task_2_3()
+
+task_2_4()
---
+++ kNN.py
@@ -0,0 +1,361 @@
+import pyreadr
+import random
+import numpy as np
+import math
+from scipy import stats
+import matplotlib.pyplot as plt
+import time
+
+
+def read_all_in(path_dot_rda, verbose=False):
+    data, data_size = read_Rda(path_dot_rda, verbose)
+    data = np.squeeze(data)
+
+    data = restore3D_Rda(data, [10, 4000, 325])
+    data = np.reshape(data, [40000, 325])
+    return data, data.shape
+
+
+
+#data structure
+#   id
+#       Xn n=(1-325) x1 = id, x>1 = pixelnr
+#           n=(0-3999) observationnr
+#               n=(0-9) GT class
+
+
+#read dumb ugly R data file
+def read_Rda(path_to_Rda, verbose=False):
+    Rdata = pyreadr.read_r(path_to_Rda) # also works for Rds
+    while len(Rdata.keys()) == 1:
+        keys = np.array(list(Rdata.keys()))
+        Rdata = Rdata[keys[0]]
+    npdata = np.array(Rdata, dtype=np.double)
+    data_size = npdata.shape
+    if verbose:
+        print("Sample size is ", npdata.shape)
+    return npdata, data_size
+
+
+#Takes a flattened list of matrices from R and restores the dimensions
+#this is neccesary because when saving the list are collapsed in C style (all of list 1, then all of list 2)
+#while the matrix is collapsed in Fortran style (first element of row 1, then first element of row 2)
+def restore3D_Rda(flattened_R_data, orig_dims):
+    restored = np.empty(orig_dims, dtype=np.double)
+    for i in range(orig_dims[0]):
+        slice = flattened_R_data[ i*orig_dims[1]*orig_dims[2] : (i+1)*orig_dims[1]*orig_dims[2]  ]
+        restored_slice = np.reshape(slice, [orig_dims[1], orig_dims[2]], order='F')
+        restored[i] = restored_slice
+    return restored
+
+
+#randomly split data into group_percentiles.size groups
+def rnd_split(arr, group_percentiles, verbose=False):
+    data_size = arr.shape[0]
+    #make array of random nummbers
+    RNJesus_array = np.random.rand(data_size)
+
+    #make mask
+    mask = np.zeros(data_size, dtype=np.int64)
+
+    sum_percentile = 0
+    c = 0
+    for groups in group_percentiles:
+        c += 1
+        sum_percentile += groups
+        delimiter = np.percentile(RNJesus_array, sum_percentile)
+        mask[RNJesus_array > delimiter] = c
+
+    out = []
+    for groups in range(c):
+        new_group = arr[mask == groups]
+        out.append(new_group)
+        if verbose:
+            print("Created group ", groups, " with ", new_group.shape[0], " elements.")
+    return out
+
+#de-mean data
+def demean(arr):
+    pass
+
+#compare predictions to ground truth
+def evaluate(prediction, ground_truth, verbose=False):
+    correct = (prediction == ground_truth.reshape(-1, 1))
+    sums = np.sum(correct, axis=0)
+    accuracy = sums / len(ground_truth)
+    if verbose:
+        print("corrects: ", sums, "/", ground_truth.size)
+        print("accuracy ", accuracy)
+    return accuracy, sums, correct
+
+#make and save plot
+def k_plot(title, k, y, yticks=(0, 101, 5), ylabel="Accuracy"):
+    plt.figure()
+    plt.plot(k, y, '--bo')
+    plt.ylabel(ylabel, fontsize=20)
+    plt.yticks(yticks)
+    plt.xlabel('k', fontsize=20)
+    plt.xticks(range(1, np.max(k)+1, 1))
+    plt.title(title + "set " + ylabel, fontsize=20)
+    plt.tick_params(labelsize=14)
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(16, 12)
+    plt.savefig(title + '_knn.png')
+
+
+#kNN class
+class kNN:
+
+
+    #"train" by saving the data for later comparison
+    def __init__(self, data):
+        self.data = data
+
+
+    #predict the classes of multiple observations
+    def mass_predict(self, pred_data, k, verbose=False):
+        predictions = np.array([self.predict(obs, k, verbose) for obs in pred_data])
+        if verbose:
+            print("Made ", predictions.shape[0] ,"predictions")
+        return predictions
+
+
+    #predict the class of an observation
+    def predict(self, obs, k, verbose=False):
+        nearest_neighbours = sorted(list(self.data), key=lambda neighbour: np.linalg.norm(obs[1:] - neighbour[1:]))
+        k_max = np.max(k)
+        nearest_k = np.array(nearest_neighbours[0:k_max])
+        classes = np.array(nearest_k[:, 0], np.int64)
+        mode = np.array([stats.mode(classes[:n]) for n in k])
+        if verbose:
+            #print("Neighbour classes were", classes)
+            print("predicted:", mode[:,0])
+        return mode[:,0]
+
+
+    #predict the classes of multiple observations
+    def mass_arg_predict(self, pred_data, k, verbose=False):
+        predictions = np.array([self.arg_predict(obs, k, verbose) for obs in pred_data])
+        if verbose:
+            print("Made ", predictions.shape[0] ,"predictions")
+        return predictions
+
+
+    #argpartition prediction
+    def arg_predict(self, obs, k, verbose=False):
+        #tttttttt = time.time()
+        #old_t = time.time()
+
+        #get distance array
+        #t = time.time()
+        diff = self.data[:,1:] - obs[1:]#.reshape(-1, 1)
+        #tt = time.time()-t
+        #print("subtracting", tt)
+
+        #ttt = time.time()
+        diff = np.linalg.norm(diff, axis=1)
+        #tttt = time.time()-ttt
+        #print("norm", tttt)
+
+        #new_t = time.time()
+        #print("calc diff", new_t-old_t)
+        #old_t = time.time()
+
+        #argpartition
+        partition = np.argpartition(diff[:], k)
+
+        #new_t = time.time()
+        #print("partition", new_t-old_t)
+        #old_t = time.time()
+
+        #get labels
+        labels = np.squeeze(self.data[:,:1])
+
+        #get labels of the nearest neighbours
+        classes = np.array([np.take(labels, partition[:_k]) for _k in k])
+
+        mode = np.array([stats.mode(classes[n]) for n in range(classes.shape[0])], dtype=np.int64)
+
+        #new_t = time.time()
+        #print("finding mode", new_t-old_t)
+
+
+        if verbose:
+            #print("Neighbour classes were", np.array(classes))
+            print("predicted:", mode[:,0])
+
+        #print("Total", time.time()-tttttttt)
+        return mode[:,0]
+
+
+##main##
+
+###1.4.1/1.4.2 - 50/50 split at varying k
+def task_1_4_12():
+    #read data
+    data, data_size = read_Rda("id100.Rda", True)
+
+    #split data
+    val, train = rnd_split(data, [50, 50], verbose=True)
+    #train kNN
+    mykNN = kNN(train)
+    k = range(1, 51)
+
+    print("Training set")
+    train_start = time.time()
+    #predictions = mykNN.mass_predict(train[:], k, verbose=False)
+    predictions = mykNN.mass_arg_predict(train[:], k, verbose=False)
+    train_end = time.time()
+    train_time = train_end - train_start
+    predictions = np.squeeze(predictions, axis=2)
+    ground_truth = np.int64(train[:, 0])
+
+    print(ground_truth.shape, predictions.shape)
+
+    accuracy, _, _ = evaluate(predictions, ground_truth, verbose=True)
+    print(accuracy[0]*100)
+
+    k_plot("Individual training ", k, accuracy*100)
+
+    print("Validation set")
+    val_start = time.time()
+    #predictions = mykNN.mass_predict(val[:], k, verbose=False)
+    predictions = mykNN.mass_arg_predict(val[:], k, verbose=False)
+    val_end = time.time()
+    val_time = val_end - val_start
+    predictions = np.squeeze(predictions)
+    ground_truth = np.int64(val[:, 0])
+
+    print(ground_truth.shape, predictions.shape)
+
+    accuracy, _, _ = evaluate(predictions, ground_truth, verbose=True)
+
+
+    k_plot("Individual validation ", k, accuracy*100)
+
+    print("Predictions for task 1.4.1 took ", int(train_time), "(training) and", int(val_time), "(validation) seconds")
+    #plt.show()
+
+
+
+
+###1.4.3 90/10 split with 10 repetitions
+def task_1_4_3():
+    accuracies = []
+    for i in range(10):
+        #read data
+        data, data_size = read_Rda("id100.Rda", verbose=False)
+        #split data
+        val, train = rnd_split(data, [10, 90], verbose=False)
+        #train kNN
+        mykNN = kNN(train)
+        k = [5]
+
+        #predictions = mykNN.mass_predict(val[:], k, verbose=False)
+        predictions = mykNN.mass_arg_predict(val[:], k, verbose=False)
+        predictions = np.squeeze(predictions, axis=2)
+        ground_truth = np.int64(val[:, 0])
+        accuracy, _, _ = evaluate(predictions, ground_truth, verbose=False)
+        accuracies.append(accuracy)
+    print("mean accuracy ", np.mean(accuracies))
+    print("std deviation", np.std(accuracies))
+
+
+###1.4.4 person independant kNN
+def task_1_4_4_all_persons():
+    print("1_4_4_all_persons")
+    #read data
+    data, data_size = read_Rda("flattened_all.Rds", True)
+    data = np.squeeze(data)
+
+    data = restore3D_Rda(data, [10, 4000, 325])
+    data = np.reshape(data, [40000, 325])
+
+    val, train = rnd_split(data, [50, 50], verbose=False)
+    val = np.array(val)
+    train = np.array(train)
+
+    #train kNN
+    mykNN = kNN(train)
+    k = range(1, 51)
+
+    """
+    print("Training set")
+    train_start = time.time()
+    #predictions = mykNN.mass_predict(train[:], k, verbose=False)
+    predictions = mykNN.mass_arg_predict(train[:], k, verbose=False)
+    train_end = time.time()
+    train_time = train_end - train_start
+
+    predictions = np.squeeze(predictions)
+    ground_truth = np.int64(train[:, 0])
+    accuracy, _, _ = evaluate(predictions, ground_truth, verbose=False)
+
+    k_plot("All persons training ", k, accuracy*100)
+    
+    """
+
+    print("Validation set")
+    val_start = time.time()
+    #predictions = mykNN.mass_predict(val[:], k, verbose=False)
+    predictions = mykNN.mass_arg_predict(val[:], k, verbose=False)
+    val_end = time.time()
+    val_time = val_end - val_start
+    predictions = np.squeeze(predictions, axis=2)
+    ground_truth = np.int64(val[:, 0])
+    accuracy, _, _ = evaluate(predictions, ground_truth, verbose=True)
+
+    k_plot("All persons validation ", k, accuracy*100, ylabel="Accuracy [%]")
+
+    print("Predictions for task 1.4.4 took ", int(val_time), "(validation) seconds")
+
+
+###1.4.5 disjust
+def task_1_4_4_disjunct():
+    print("1_4_4_disjunct")
+    #read data
+    data, data_size = read_Rda("flattened_all.Rds", True)
+    np.squeeze(data)
+
+    data = restore3D_Rda(data, [10, 4000, 325])
+    data = np.reshape(data, [40000, 325])
+
+    val   = data[:int(data.shape[0]/2) , :]
+    train = data[int(data.shape[0]/2): , :]
+    val = np.array(val)
+    train = np.array(train)
+
+    #train kNN
+    mykNN = kNN(train)
+    k = range(1, 51)
+
+    """
+    print("Training set")
+    predictions = mykNN.mass_predict(train[:], k, verbose=False)
+    predictions = np.squeeze(predictions)
+    ground_truth = np.int64(train[:, 0])
+    accuracy, _, _ = evaluate(predictions, ground_truth, verbose=False)
+    
+    k_plot("Disjunct persons validation ", k, accuracy*100)
+    """
+
+    print("Validation set")
+    #predictions = mykNN.mass_predict(val[:], k, verbose=False)
+    predictions = mykNN.mass_arg_predict(val[:], k, verbose=False)
+    predictions = np.squeeze(predictions, axis=2)
+    ground_truth = np.int64(val[:, 0])
+    accuracy, _, _ = evaluate(predictions, ground_truth, verbose=True)
+
+    k_plot("Disjunct validation ", k, accuracy*100, ylabel="Accuracy [%]")
+
+
+###########################
+
+
+#set seed
+np.random.seed(423)
+#run tasks
+#task_1_4_12()
+#task_1_4_3()
+#task_1_4_4_all_persons()
+#task_1_4_4_disjunct()
